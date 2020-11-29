3894e58a73c600bbe78b04c171e3a9892e96c2cf
Andre <kappaj@gmail.com>
2020-11-29 11:14:37 +0200

    main: initial commit

---
+++ freshdesk/src/main/java/za/co/sfh/freshdesk/FreshdeskApplication.java
@@ -0,0 +1,13 @@
+package za.co.sfh.freshdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshdeskApplication.class, args);
+	}
+
+}
---
+++ freshdesk/src/test/java/za/co/sfh/freshdesk/FreshdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.sfh.freshdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
